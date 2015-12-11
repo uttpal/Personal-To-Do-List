adcc5e483d355678a1d6749c0d0a42787fc1ee67
Kumar Uttpal <uttpal25993@gmail.com>
2015-12-12 02:17:43 +0530

    implemened todo delete

--- routes/todo.js
+++ routes/todo.js
@@ -1,9 +1,13 @@
 //routes/todo.js
 
+//load user mongoose model
 var Todo = require('../app/models/todo.js');
 
+//todo app homepage
 exports.home = function ( req, res, next ){
 
+  //Find all todos of current user
+  //sort according to creation time and return
   Todo.
     find({ email : req.user.email }).
     sort( '-updated_at' ).
@@ -11,21 +15,37 @@ exports.home = function ( req, res, next ){
       if( err ) return next( err );
 
       res.render( 'todo', {
-          title : 'Personal To-Do App',
           todos : todos
       });
     });
 };
 
+//create new task
 exports.create = function ( req, res, next ){
+  //takes user parameter and set it to new todo object 
   new Todo({
       email 	 : req.user.email,
       content    : req.body.content,
       updated_at : Date.now()
-  }).save( function ( err, todo, count ){
+  }).save( function ( err, todo, count ){     //saves new task to DB.
     if( err ) return next( err );
 
     res.redirect( '/todo' );
+  });
+};
+
+
+//deletes the task
+exports.destroy = function ( req, res, next ){
+  //find task by supplied id
+  Todo.findById( req.params.id, function ( err, todo ){
+    
+    //deletes from database
+    todo.remove( function ( err, todo ){
+      if( err ) return next( err );
+
+      res.redirect( '/todo' );
+    });
   });
 };
 
